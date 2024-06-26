628e68b9fba039d1c1869286d7dd8b23520d3c25
Martin Stolle <martin.stolle@zeit.de>
2024-06-26 12:02:19 +0200

    Add sqlalchemy layer
    maybe it is the combination of both

--- backend/app.py
+++ backend/app.py
@@ -1,37 +1,41 @@
-import psycopg2
+from sqlalchemy import create_engine, select, MetaData, Table, Column, Integer, CHAR, ForeignKey, PrimaryKeyConstraint
 import logging
 import random
+
+
+metadata = MetaData()
+
+current_object = Table('current_object', metadata,
+    Column('zoid', Integer, primary_key=True),
+    Column('tid', Integer)
+)
+
+object_state = Table('object_state', metadata,
+    Column('zoid', Integer, ForeignKey('current_object.zoid')),
+    Column('tid', Integer),
+    Column('state', CHAR),
+    PrimaryKeyConstraint('zoid', 'tid')
+)
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 log = logging.getLogger(__name__)
 
-
 def continuously_execute():
-    conn_params = {
-        "dbname": "example",
-        "user": "postgres",
-        "password": "example",
-        "host": "db"
-    }
+    db_url = "postgresql+psycopg2://postgres:example@db/example"
+    engine = create_engine(db_url)
+    connection = engine.connect()
 
     try:
-        conn = psycopg2.connect(**conn_params)
-        cursor = conn.cursor()
         log.info('Fetching all zoid values')
-        cursor.execute("SELECT zoid FROM current_object")
-        all_zoids = cursor.fetchall() 
+        all_zoids = connection.execute(select(current_object)).all()
 
         i = 0
         while True:
             if all_zoids:
-                random_zoid = random.choice(all_zoids)[0] 
-                cursor.execute("""
-                    SELECT co.zoid, co.tid, os.state 
-                    FROM current_object co
-                    JOIN object_state os ON co.zoid = os.zoid
-                    WHERE co.zoid = %s
-                """, (random_zoid,))
-                info = cursor.fetchone()
+                random_zoid = random.choice(all_zoids)[0]
+                query = select(current_object.c.zoid).where(current_object.c.zoid == random_zoid)
+
+                info = connection.execute(query).fetchone()
                 i += 1
                 if i % 10000 == 0:
                     i = 0
@@ -39,10 +43,9 @@ def continuously_execute():
             else:
                 log.info('No zoids found in current_object')
     except Exception as e:
-        print(f"Failed to connect to the database: {e}")
+        log.error(f"Failed to connect to the database: {e}")
     finally:
-        cursor.close()
-        conn.close()
+        connection.close()
 
 if __name__ == "__main__":
     continuously_execute()
